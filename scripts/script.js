c43b47ec244dbfb51996e6261b05dc29affde45f
Vadim Lavorchuk <lavorchukvadim@yandex.ua>
2015-08-28 14:48:50 +0300

    refactor html and javascript code (functional way)

--- scripts/script.js
+++ scripts/script.js
@@ -7,27 +7,26 @@ var start_button = document.getElementById('start'),
     msec = 0,
     sec = 0,
     min = 0,
-    timeout;
+    timeout,
+    message = document.getElementsByClassName('message')[0],
+    msg = '';
 
 function add() {
   msec++;
-  if (msec >= 10) {
+  if (msec >= 100) {
     msec = 0;
     sec++;
     if (sec >= 60) {
       sec = 0;
+      min++;
     }
   }
   timer();
 }
-
-function stop_show_reset () {
-
-}
-
 function timer() {
-  timeout = setTimeout(add, 100);
+  timeout = setTimeout(add, 10);
 }
+
 function start_animate() {
   clock_min.classList.add('run', 'animate');
   clock_sec.classList.add('run', 'animate');
@@ -37,24 +36,40 @@ function stop_animate() {
   clock_sec.classList.remove('run');
 }
 function reset_animate() {
-    clock_min.classList.remove('animate');
-    clock_sec.classList.remove('animate');
+  clock_min.classList.remove('animate');
+  clock_sec.classList.remove('animate');
+}
+function get_time() {
+  message.classList.add('show');
+  msg ='<p>'+
+      (min ? (min > 9 ? min : '0' + min) : '00')
+      +'m : '+
+      (sec ? (sec > 9 ? sec : '0' + sec) : '00')
+      +'s : '+
+      (msec ? (msec > 9 ? msec : '0' + msec) : '00')
+      +'ms</p>';
+  message.innerHTML = msg;
+}
+function reset_time() {
+  msec = sec = min = 0;
 }
 
 stop_button.disabled = true;
 
 start_button.addEventListener('click', function() {
-  reset_animate();
-  start_animate();
-  timer();
+  reset_time();
+  message.classList.remove('show');
   start_button.disabled = true;
   stop_button.disabled = false;
+  start_animate();
+  timer();
 });
 
 stop_button.addEventListener('click', function(){
   stop_animate();
-  window.alert(min+' : '+sec+' : '+msec);
+  get_time();
+  clearTimeout(timeout);
   start_button.disabled = false;
   stop_button.disabled = true;
   reset_animate();
-}, false)
+})
