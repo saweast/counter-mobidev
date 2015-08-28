8a157c781e256d2d45fa3f7dabc61dc9c641ef1f
Vadim Lavorchuk <lavorchukvadim@yandex.ua>
2015-08-28 16:59:20 +0300

    refactor

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,9 @@
 "use strict";
 
-var start_button = document.getElementById('start'),
-    stop_button = document.getElementById('stop'),
-    clock_min = document.getElementById('minutes'),
-    clock_sec = document.getElementById('seconds'),
+var startButton = document.getElementById('start'),
+    stopButton = document.getElementById('stop'),
+    clockMin = document.getElementById('minutes'),
+    clockSec = document.getElementById('seconds'),
     msec = 0,
     sec = 0,
     min = 0,
@@ -13,10 +13,10 @@ var start_button = document.getElementById('start'),
 
 function add() {
   msec++;
-  if (msec >= 100) {
+  if (msec > 9) {
     msec = 0;
     sec++;
-    if (sec >= 60) {
+    if (sec > 59) {
       sec = 0;
       min++;
     }
@@ -24,52 +24,48 @@ function add() {
   timer();
 }
 function timer() {
-  timeout = setTimeout(add, 10);
+  timeout = setTimeout(add, 100);
 }
-
-function start_animate() {
-  clock_min.classList.add('run', 'animate');
-  clock_sec.classList.add('run', 'animate');
+function startAnimate() {
+  clockMin.classList.add('run', 'animate');
+  clockSec.classList.add('run', 'animate');
 }
-function stop_animate() {
-  clock_min.classList.remove('run');
-  clock_sec.classList.remove('run');
+function stopAnimate() {
+  clockMin.classList.remove('run');
+  clockSec.classList.remove('run');
 }
-function reset_animate() {
-  clock_min.classList.remove('animate');
-  clock_sec.classList.remove('animate');
+function resetAnimate() {
+  clockMin.classList.remove('animate');
+  clockSec.classList.remove('animate');
 }
-function get_time() {
+function getTime() {
   message.classList.add('show');
-  msg ='<p>'+
-      (min ? (min > 9 ? min : '0' + min) : '00')
-      +'m : '+
-      (sec ? (sec > 9 ? sec : '0' + sec) : '00')
-      +'s : '+
-      (msec ? (msec > 9 ? msec : '0' + msec) : '00')
-      +'ms</p>';
+  msg ='<p>'+(min ? (min > 9 ? min : '0' + min) : '00')+'m : '+
+    (sec ? (sec > 9 ? sec : '0' + sec) : '00')+'s : '+
+    (msec ? (msec > 9 ? msec : '0' + msec) : '00')+'ms</p>';
   message.innerHTML = msg;
 }
-function reset_time() {
+function resetTime() {
   msec = sec = min = 0;
 }
-
-stop_button.disabled = true;
-
-start_button.addEventListener('click', function() {
-  reset_time();
+function startCount() {
+  resetTime();
   message.classList.remove('show');
-  start_button.disabled = true;
-  stop_button.disabled = false;
-  start_animate();
+  startButton.disabled = true;
+  stopButton.disabled = false;
+  startAnimate();
   timer();
-});
-
-stop_button.addEventListener('click', function(){
-  stop_animate();
-  get_time();
+}
+function stopCount() {
+  stopAnimate();
+  getTime();
   clearTimeout(timeout);
-  start_button.disabled = false;
-  stop_button.disabled = true;
-  reset_animate();
-})
+  startButton.disabled = false;
+  stopButton.disabled = true;
+  resetAnimate();
+}
+
+stopButton.disabled = true;
+
+startButton.addEventListener('click', startCount, false);
+stopButton.addEventListener('click', stopCount, false);
